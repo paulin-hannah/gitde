dc9e48d5bc66638c3775f329afe1f67fafbde829
paulin <vivin.rufus@gmail.com>
2023-04-27 14:18:07 +0530

    day7

--- guvipra/jesus.js
+++
@@ -1,9 +0,0 @@
-function reqListener() {
-    console.log(this.responseText);
-  }
-  
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", reqListener);
-  req.open("GET", "https://restcountries.eu/rest/v2/all");
-  req.send();
-  
